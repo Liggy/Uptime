fc7eb185668a6ddea4ee9a4fb17aaede88ca784f
Liggy <2-liggy@users.noreply.git.liggy.de>
2025-11-06 15:14:55 +0100

    Add further string table constants

--- Common/resource.h
+++ Common/resource.h
@@ -1,12 +1,24 @@
 //{{NO_DEPENDENCIES}}
 #define IDS_HOWTO                       101
 #define IDS_UP_SINCE                    102
+#define IDS_UPTIME_REPORT               103
+#define IDS_CURRENT_OS                  104
+#define IDS_TIME_ZONE                   105
+#define IDS_EXECUTION_TIMESTAMP         106
+#define IDS_UPTIME_LIST_HEADER          107
+#define IDS_EVENT_BOOT                  108
+#define IDS_EVENT_SHUTDOWN              109
+#define IDS_EVENT_CRASH                 110
+#define IDS_EVENT_HIBERNATE             111
+#define IDS_EVENT_HYBRID_SHUTDOWN       112
+#define IDS_EVENT_WAKEUP                113
+
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        114
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101

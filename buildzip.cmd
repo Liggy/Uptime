d744c2bd59eb3bb12d6232a48f4dd6a1dc859121
Liggy <2-liggy@users.noreply.git.liggy.de>
2026-05-06 00:42:56 +0200

    Add script to build archive
    Add archive directory to .gitignore

---
+++ buildzip.cmd
@@ -0,0 +1,15 @@
+@echo off
+if not exist %~dp0Dist\nul (
+	md %~dp0Dist
+	pushd %~dp0dist
+) else (
+	pushd %~dp0dist
+	del uptime_arm64.zip uptime_x64.zip uptime_x86.zip 2> nul
+)
+cd %~dp0ARM64\Release
+zip -R ..\..\dist\uptime_arm64.zip uptime.exe uptime.dll *.mui
+cd %~dp0x64\Release
+zip -R ..\..\dist\uptime_x64.zip uptime.exe uptime.dll *.mui
+cd %~dp0Release
+zip -R ..\dist\uptime_x86.zip uptime.exe uptime.dll *.mui
+popd

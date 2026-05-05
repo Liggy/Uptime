fe2cded8039376a03aeb04cbbec2d8b4789aca10
Liggy <2-liggy@users.noreply.git.liggy.de>
2026-05-06 00:20:38 +0200

    Extend I18N class
    Add ProcessMessageToBuffer and LoadString methods

--- Uptime/I18N.cpp
+++ Uptime/I18N.cpp
@@ -1,16 +1,19 @@
 #include "pch.h"
 #include "I18N.h"
 
+
 I18N::I18N()
 {
 	hResFile = nullptr;
 	hCallback = nullptr;
 }
 
-I18N::I18N(CallbackPrototype output)
+
+I18N::I18N(i18nCallbackPrototype cbOutput)
 {
-	Init(output);
+	Init(cbOutput);
 }
+
 
 I18N::~I18N()
 {
@@ -18,15 +21,17 @@ I18N::~I18N()
 		FreeLibrary(hResFile);
 }
 
-void I18N::Init(CallbackPrototype output)
+
+void I18N::Init(i18nCallbackPrototype cbOutput)
 {
-	hResFile = LoadLibraryEx(_T("Uptime.dll"), NULL, LOAD_LIBRARY_AS_IMAGE_RESOURCE | LOAD_LIBRARY_AS_DATAFILE);
-	hCallback = output;
+	hResFile = ::LoadLibraryEx(_T("Uptime.dll"), NULL, LOAD_LIBRARY_AS_IMAGE_RESOURCE | LOAD_LIBRARY_AS_DATAFILE);
+	hCallback = cbOutput;
 }
+
 
 int I18N::ProcessMessage(UINT uID, ...) const
 {
-	TCHAR buffer[1024];
+	TCHAR lpBuffer[1024];
 	va_list arg_ptr;
 	int rc;
 	
@@ -34,12 +39,40 @@ int I18N::ProcessMessage(UINT uID, ...) const
 		return 0;
 
 	va_start(arg_ptr, uID);
-	LoadString(hResFile, uID, buffer, 1024);
+	::LoadString(hResFile, uID, lpBuffer, 1024);
 
-	rc = hCallback(buffer, arg_ptr);
+	rc = hCallback(lpBuffer, arg_ptr);
 
 	va_end(arg_ptr);
 
 	return rc;
 }
 
+
+int I18N::ProcessMessageToBuffer(TCHAR* lpDestination, UINT uDestinationSize, UINT uID, ...) const
+{
+	TCHAR lpBuffer[1024];
+	va_list arg_ptr;
+	int rc;
+
+	if (!hResFile || !hCallback)
+		return 0;
+
+	va_start(arg_ptr, uID);
+	::LoadString(hResFile, uID, lpBuffer, 1024);
+
+	rc = _vstprintf_p(lpDestination, uDestinationSize, lpBuffer, arg_ptr);
+
+	va_end(arg_ptr);
+
+	return rc;
+}
+
+
+int I18N::LoadString(UINT uID, LPTSTR lpBuffer, int cchBufferMax) const
+{
+	if (!hResFile)
+		return 0;
+	
+	return ::LoadString(hResFile, uID, lpBuffer, cchBufferMax);
+}
--- Uptime/I18N.h
+++ Uptime/I18N.h
@@ -1,18 +1,22 @@
 #pragma once
-typedef int (*CallbackPrototype)(const TCHAR*, va_list);
+typedef int (*i18nCallbackPrototype)(const TCHAR*, va_list);
 
 class I18N
 {
 	HMODULE hResFile;
-	CallbackPrototype hCallback;
+	i18nCallbackPrototype hCallback;
 
 public:
 	I18N();
-	I18N(CallbackPrototype output);
+	I18N(i18nCallbackPrototype cbOutput);
 	~I18N();
 
-	void Init(CallbackPrototype output);
+	void Init(i18nCallbackPrototype cbOutput);
 
 
 	int ProcessMessage(UINT, ...) const;
+	int ProcessMessageToBuffer(TCHAR*, UINT, UINT, ...) const;
+
+	int LoadString(UINT uID, LPTSTR lpBuffer,int cchBufferMax) const;
+
 };
